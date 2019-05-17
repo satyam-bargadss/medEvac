5b739c9f3ac500f2a0b74ede679c1af17472d535
satyam-bargadss <satyam@bargadss.com>
2019-05-17 17:50:26 +0530

    working in service controller creating seeder to check

--- app/service.php
+++ app/service.php
@@ -4,7 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class service extends Model
 {
-    //
+    protected $primaryKey = 'serviceId';
+    protected $fillable = ['serviceName', 'serviceDesc'];
 }
